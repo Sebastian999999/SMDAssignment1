7b715a19b00b9196a251dc60709c58a400cb1929
Sebastian999999 <i191994@nu.edu.pk>
2024-02-06 14:01:48 +0500

    "first commit"

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "i191994"
+include(":app")
+ 
